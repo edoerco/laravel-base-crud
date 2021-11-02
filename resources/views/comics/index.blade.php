5dfe5cf36fb52b664ae24708a538afe568341d7e
Edoardo <ercolino.edoardo@gmail.com>
2021-11-02 18:30:42 +0100

    conclusione dell'index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,5 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="mt-4">Indice dei fumetti</h1>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Title</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Series</th>
+                            <th scope="col">Sale Date</th>
+                            <th scope="col">Type</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($data as $comic)
+                            <tr>
+                                <th scope="row">{{ $comic['id'] }}</th>
+                                <td>{{ $comic['title'] }}</td>
+                                <td>{{ $comic['description'] }}</td>
+                                <td>{{ $comic['price'] }}</td>
+                                <td>{{ $comic['series'] }}</td>
+                                <td>{{ $comic['sale_date'] }}</td>
+                                <td>{{ $comic['type'] }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 @endsection
